cd39b6fa1bb51642f539fdae7898f8d36122adf4
sMarianut <smarianut@gmail.com>
2023-07-06 22:55:15 +0200

    Sprint_2_Mariano_Silva

---
+++ assets/scripts/upcoming.js
@@ -0,0 +1,35 @@
+let cards = document.getElementById("cartas")
+let datos = data.events
+let current = data.currentDate
+function createModel(evento){
+    return `<div class="card shadow p-3 bg-success-subtle" style="width: 15rem;">
+    <img src="${evento.image}" class="card-img-top" alt="Image of: ${evento.name}">
+    <div class="card-body">
+        <h6 class="card-title h-50">${evento.name}</h6>
+        <p class="card-text">${evento.description}</p>
+        <div class="d-flex justify-content-between">
+            <p class="card-text d-inline">$${evento.price}</p>
+            <a href="../pages/details.html" class="btn btn-primary">Details</a>
+        </div>
+    </div>
+</div>`
+}
+function printModel(events) {
+   for(let evento of events){
+     cards.innerHTML += createModel(evento)
+   }
+}
+
+
+function filterEvents( events, dataCurrent ){
+    let filteredEvents = []
+    
+    for (let evento of datos) {
+        if( evento.date > dataCurrent){
+            filteredEvents.push( evento )
+        }
+    }
+    return filteredEvents
+}
+let filteredEvents = filterEvents(data.events, current)
+printModel(filteredEvents, cards)
---
+++ main.js
@@ -0,0 +1,38 @@
+// let section = document.getElementById("cartas")
+let cards = document.getElementById("cartas")
+let datos = data.events
+function createModel(evento){
+    return `<div class="card shadow p-3 bg-success-subtle" style="width: 15rem;">
+    <img src="${evento.image}" class="card-img-top" alt="Image of: ${evento.name}">
+    <div class="card-body">
+        <h6 class="card-title h-50">${evento.name}</h6>
+        <p class="card-text">${evento.description}</p>
+        <div class="d-flex justify-content-between">
+            <p class="card-text d-inline">$${evento.price}</p>
+            <a href="./assets/pages/details.html" class="btn btn-primary">Details</a>
+        </div>
+    </div>
+</div>`
+}
+function printModel(events) {
+   for(let evento of events){
+     cards.innerHTML += createModel(evento)
+   }
+}
+function filter(events) {
+    for(let evento of events){
+      cards.innerHTML += createModel(evento)
+    }
+}
+printModel(datos)
+
+
+//function filtrarMentores( listaOriginal, modulo ){
+//    const mentoresFiltrados = []
+//   for (const mentor of listaOriginal) {
+//       if(mentor.modulo == modulo){
+//           mentoresFiltrados.push( mentor )
+//       }
+//   }
+//  return mentoresFiltrados
+//}
