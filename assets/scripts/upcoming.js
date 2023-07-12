3b92708d86773cb94e972844a31f09432f7c7823
sMarianut <smarianut@gmail.com>
2023-07-12 17:19:11 +0200

    Agrege filtro de chekcs, falta filtro de busqueda

--- assets/scripts/upcoming.js
+++ assets/scripts/upcoming.js
@@ -1,6 +1,10 @@
 let cards = document.getElementById("cartas")
 let datos = data.events
 let current = data.currentDate
+let cat = datos.map(category => category.category)
+let newCat = new Set(cat)
+let categorias = Array.from(newCat)
+let checkbox = document.getElementById("buscador")
 function createModel(evento) {
     return `<div class="card shadow p-3 bg-success-subtle" style="width: 15rem;">
     <img src="${evento.image}" class="card-img-top" alt="Image of: ${evento.name}">
@@ -20,7 +24,6 @@ function printModel(events) {
     }
 }
 
-
 function filterEvents(events, dataCurrent) {
     let filteredEvents = []
 
@@ -32,4 +35,56 @@ function filterEvents(events, dataCurrent) {
     return filteredEvents
 }
 let filteredEvents = filterEvents(data.events, current)
-printModel(filteredEvents, cards)
+printModel(filteredEvents)
+
+
+
+
+
+
+
+
+
+
+
+//CheckBoxes
+
+
+function createCheck(check) {
+    return `<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="${check}" id="${check}">
+    <label class="form-check-label" for="${check}">
+    ${check}
+    </label>`
+}
+
+function printCheckBox(cat, cont) {
+    for (const elemento of cat) {
+        cont.innerHTML += createCheck(elemento)
+    }
+}
+printCheckBox(categorias, checkbox)
+console.log(checkbox)
+
+
+checkbox.addEventListener('change', () => {
+    cards.innerHTML = ""
+    let boxCheck = document.querySelectorAll("input[type='checkbox']:checked")
+    let arrayCheck = []
+    boxCheck.forEach((values) => arrayCheck.push(values.value))
+    let catFilter = datos.filter(event => arrayCheck.includes(event.category) || arrayCheck.length == 0)
+    printModel(catFilter, cards)
+})
+
+// InputSearch
+let containerSearch = document.getElementById("contenedorSearch")
+let searchInput = document.getElementById("buscador2")
+searchInput.addEventListener('input', () => {
+    const value = showValue(searchInput)
+    let nombre = datos.filter(dato => dato.name.toLowerCase().startsWith(value))
+    return nombre
+})
+function showValue(input) {
+    let valorInputSearch = input.value
+    return valorInputSearch
+}
--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // let section = document.getElementById("cartas")
 let cards = document.getElementById("cartas")
 let datos = data.events
+let cat = datos.map(category => category.category)
+let newCat = new Set(cat)
+let categorias = Array.from(newCat)
+let checkbox = document.getElementById("buscador")
 function createModel(evento) {
   return `<div class="card shadow p-3 bg-success-subtle" style="width: 15rem;">
     <img src="${evento.image}" class="card-img-top" alt="Image of: ${evento.name}">
@@ -14,16 +18,70 @@ function createModel(evento) {
     </div>
 </div>`
 }
-function printModel(events) {
-  for (let evento of events) {
-    cards.innerHTML += createModel(evento)
+function printModel(events, ubicacion) {
+  let card = ''
+  if (events.length == 0) {
+    card += `<p class="display-1">No coincidences</P>`
+    ubicacion.innerHTML = cards
+    return false
+  } else {
+    ubicacion.innerHTML = ''
+    for (let evento of events) {
+      ubicacion.innerHTML += createModel(evento)
+    }
   }
 }
-function filter(events) {
-  for (let evento of events) {
-    cards.innerHTML += createModel(evento)
+printModel(datos, cards)
+
+// InputSearch
+let searchInput = document.getElementById("inputSearch")
+searchInput.addEventListener('input', () => {
+  const value = showValue(searchInput)
+  let nombre = datos.filter(dato => dato.name.toLowerCase().startsWith(value))
+  return nombre
+})
+function showValue(input) {
+  let valorInputSearch = input.value
+  return valorInputSearch
+}
+
+
+
+
+
+
+
+
+
+
+//CheckBoxes
+
+
+function createCheck(check) {
+  return `<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="${check}" id="${check}">
+  <label class="form-check-label" for="${check}">
+      ${check}
+  </label>`
+}
+
+function printCheckBox(cat, cont) {
+  for (const elemento of cat) {
+    cont.innerHTML += createCheck(elemento)
   }
 }
-printModel(datos)
+printCheckBox(categorias, checkbox)
+
+
+checkbox.addEventListener('change', () => {
+  cards.innerHTML = ""
+  let boxCheck = document.querySelectorAll("input[type='checkbox']:checked")
+  let arrayCheck = []
+  boxCheck.forEach((values) => arrayCheck.push(values.value))
+  let catFilter = datos.filter(event => arrayCheck.includes(event.category) || arrayCheck.length == 0)
+  printModel(catFilter, cards)
+})
+
+
 
 
